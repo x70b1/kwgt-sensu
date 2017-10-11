ba9260cb30b7f91861c978c77ba281b7eb515534
x70b1 <x70b1@users.noreply.github.com>
2017-10-11 22:21:58 +0200

    add lightweight-widget

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-#  build kwgt-sensu-events
-if [ -d "kwgt-sensu-events" ]; then
-    cd kwgt-sensu-events/ || exit
+#  build kwgt-sensu-events-lightweight
+if [ -d "kwgt-sensu-events-lightweight" ]; then
+    cd kwgt-sensu-events-lightweight/ || exit
 
-    zip -r sensu-events.kwgt *
-    mv sensu-events.kwgt ../
+    zip -r sensu-events-lightweight.kwgt *
+    mv sensu-events-lightweight.kwgt ../
+
+    cd ..
+fi
+
+
+#  build kwgt-sensu-events-fullfeatured
+if [ -d "kwgt-sensu-events-fullfeatured" ]; then
+    cd kwgt-sensu-events-fullfeatured/ || exit
+
+    zip -r sensu-events-fullfeatured.kwgt *
+    mv sensu-events-fullfeatured.kwgt ../
 
     cd ..
 fi
